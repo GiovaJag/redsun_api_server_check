56415c299b058e86553f6f2378a759c56d30cf42
GiovaJag <58421253+GiovaJag@users.noreply.github.com>
2023-07-08 21:34:38 +0200

    1.3 version

--- apired_1.py
+++ apired_1.py
@@ -17,29 +17,48 @@ prev_stringtext = ""
 def getapiredsun():
     response = requests.get(url, headers=head)
     if response.ok:
-       json_response = response.json()
-       current_gm_name = json_response["results"][0]["currentmap"]["gamemode"]["name"]
-       current_name_map = json_response["results"][0]["currentmap"]["map"]["name"]
-       current_usa_gm_name = json_response["results"][1]["currentmap"]["gamemode"]["name"]
-       current_usa_name_map = json_response["results"][1]["currentmap"]["map"]["name"]
-       if json_response['results'][0]['nextmap'] is not None and 'gamemode' in json_response['results'][0]['nextmap'] and 'name' in json_response['results'][0]['nextmap']['gamemode'] and 'map' in json_response['results'][0]['nextmap'] and 'name' in json_response['results'][0]['nextmap']['map']:
-            next_gm_name = json_response["results"][0]["nextmap"]["gamemode"]["name"]
-            next_map_name = json_response["results"][0]["nextmap"]["map"]["name"]
-       else:
-            next_gm_name = "Not set"
-            next_map_name = "Not set"     
-       #print("\n")
-       if json_response['results'][1]['nextmap'] is not None and 'gamemode' in json_response['results'][1]['nextmap'] and 'name' in json_response['results'][1]['nextmap']['gamemode'] and 'map' in json_response['results'][1]['nextmap'] and 'name' in json_response['results'][1]['nextmap']['map']:
-            next_usa_gm_name = json_response["results"][1]["nextmap"]["gamemode"]["name"]
-            next_usa_map_name = json_response["results"][1]["nextmap"]["map"]["name"]
-       else:
-            next_usa_gm_name = "Not set"
-            next_usa_map_name = "Not set"
-       stringtext = "*RAIDEN*\n" + current_gm_name + " - " + current_name_map + "\n" + "- - - - - - nextmap - - - - -\n" + next_gm_name + " - " + next_map_name + "\n\n*ARMSTRONG*\n" + current_usa_gm_name + " - " + current_usa_name_map + "\n- - - - - - nextmap - - - - - -\n" + next_usa_gm_name + " - " + next_usa_map_name
-       return stringtext
+        json_response = response.json()
 
+        if json_response is not None and 'results' in json_response:
+            results = json_response['results']
+
+            if results is not None and len(results) >= 2:
+                if 'currentmap' in results[0] and 'gamemode' in results[0]['currentmap'] and 'name' in results[0]['currentmap']['gamemode'] and 'map' in results[0]['currentmap'] and 'name' in results[0]['currentmap']['map']:
+                    current_gm_name = results[0]["currentmap"]["gamemode"]["name"]
+                    current_name_map = results[0]["currentmap"]["map"]["name"]
+                else:
+                    current_gm_name = "Not set"
+                    current_name_map = "Not set"
+
+                if results[1] is not None and 'currentmap' in results[1] and results[1]['currentmap'] is not None and 'gamemode' in results[1]['currentmap'] and 'name' in results[1]['currentmap']['gamemode'] and 'map' in results[1]['currentmap'] and 'name' in results[1]['currentmap']['map']:
+                    current_usa_gm_name = results[1]["currentmap"]["gamemode"]["name"]
+                    current_usa_name_map = results[1]["currentmap"]["map"]["name"]
+                else:
+                    current_usa_gm_name = "Not set"
+                    current_usa_name_map = "Not set"
+
+                if 'nextmap' in results[0] and results[0]['nextmap'] is not None and 'gamemode' in results[0]['nextmap'] and 'name' in results[0]['nextmap']['gamemode'] and 'map' in results[0]['nextmap'] and 'name' in results[0]['nextmap']['map']:
+                    next_gm_name = results[0]["nextmap"]["gamemode"]["name"]
+                    next_map_name = results[0]["nextmap"]["map"]["name"]
+                else:
+                    next_gm_name = "Not set"
+                    next_map_name = "Not set"
+
+                if results[1] is not None and 'nextmap' in results[1] and results[1]['nextmap'] is not None and 'gamemode' in results[1]['nextmap'] and 'name' in results[1]['nextmap']['gamemode'] and 'map' in results[1]['nextmap'] and 'name' in results[1]['nextmap']['map']:
+                    next_usa_gm_name = results[1]["nextmap"]["gamemode"]["name"]
+                    next_usa_map_name = results[1]["nextmap"]["map"]["name"]
+                else:
+                    next_usa_gm_name = "Not set"
+                    next_usa_map_name = "Not set"
+
+                stringtext = "*RAIDEN*\n" + current_gm_name + " - " + current_name_map + "\n" + "- - - - - - nextmap - - - - -\n" + next_gm_name + " - " + next_map_name + "\n\n*ARMSTRONG*\n" + current_usa_gm_name + " - " + current_usa_name_map + "\n- - - - - - nextmap - - - - - -\n" + next_usa_gm_name + " - " + next_usa_map_name
+                return stringtext
+            else:
+                print("Results not available in the JSON response.")
+        else:
+            print("JSON response does not contain the necessary data.")
     else:
-       print(response.content)
+        print(response.content)
 
 def send_to_telegram():
     global prev_stringtext
@@ -62,7 +81,7 @@ def send_to_telegram():
             #alert_request3 = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_alert_chatID + '&parse_mode=Markdown&text=' + 'feed and seed' #the last string is the text sent for notification
             #requests.get(alert_request3)
     else:
-        response = None
+        response = ""
 
 def checktime():
     timecurrent = datetime.datetime.now()
