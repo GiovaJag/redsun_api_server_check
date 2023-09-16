598b2beae680d427eb3211d01367ab6f89a06ad0
GiovaJag <58421253+GiovaJag@users.noreply.github.com>
2023-09-16 08:54:34 +0200

    v 1.4
    added "current map filename"

--- apired_1.py
+++ apired_1.py
@@ -26,16 +26,22 @@ def getapiredsun():
                 if 'currentmap' in results[0] and 'gamemode' in results[0]['currentmap'] and 'name' in results[0]['currentmap']['gamemode'] and 'map' in results[0]['currentmap'] and 'name' in results[0]['currentmap']['map']:
                     current_gm_name = results[0]["currentmap"]["gamemode"]["name"]
                     current_name_map = results[0]["currentmap"]["map"]["name"]
+                    current_filename = results[0]["currentmap"]["map"]["filename"]
+                    current_filename = current_filename.replace("_", " ").replace(".", " ").replace("workshop/", "")
                 else:
                     current_gm_name = "Not set"
                     current_name_map = "Not set"
+                    current_filename = "Not set"
 
                 if results[1] is not None and 'currentmap' in results[1] and results[1]['currentmap'] is not None and 'gamemode' in results[1]['currentmap'] and 'name' in results[1]['currentmap']['gamemode'] and 'map' in results[1]['currentmap'] and 'name' in results[1]['currentmap']['map']:
                     current_usa_gm_name = results[1]["currentmap"]["gamemode"]["name"]
                     current_usa_name_map = results[1]["currentmap"]["map"]["name"]
+                    current_usa_filename = results[1]["currentmap"]["map"]["filename"]
+                    current_usa_filename = current_usa_filename.replace("_", " ").replace(".", " ").replace("workshop/", "")
                 else:
                     current_usa_gm_name = "Not set"
                     current_usa_name_map = "Not set"
+                    current_usa_filename = "Not set"
 
                 if 'nextmap' in results[0] and results[0]['nextmap'] is not None and 'gamemode' in results[0]['nextmap'] and 'name' in results[0]['nextmap']['gamemode'] and 'map' in results[0]['nextmap'] and 'name' in results[0]['nextmap']['map']:
                     next_gm_name = results[0]["nextmap"]["gamemode"]["name"]
@@ -51,7 +57,8 @@ def getapiredsun():
                     next_usa_gm_name = "Not set"
                     next_usa_map_name = "Not set"
 
-                stringtext = "*RAIDEN*\n" + current_gm_name + " - " + current_name_map + "\n" + "- - - - - - nextmap - - - - -\n" + next_gm_name + " - " + next_map_name + "\n\n*ARMSTRONG*\n" + current_usa_gm_name + " - " + current_usa_name_map + "\n- - - - - - nextmap - - - - - -\n" + next_usa_gm_name + " - " + next_usa_map_name
+                stringtext = "*RAIDEN*\n" + current_gm_name + "   " + current_name_map + "\n" + current_filename + "\n              - nextmap -        \n" + next_gm_name + "   " + next_map_name + "\n\n" + "*ARMSTRONG*\n" + current_usa_gm_name + "   " + current_usa_name_map + "\n" + current_usa_filename + "\n               - nextmap -       \n" + next_usa_gm_name + "   " + next_usa_map_name
+                #stringtext = "*RAIDEN*\n" + current_gm_name + " - " + current_name_map + "\n" + "- - - - - - nextmap - - - - -\n" + next_gm_name + " - " + next_map_name + "\n\n*ARMSTRONG*\n" + current_usa_gm_name + " - " + current_usa_name_map + "\n- - - - - - nextmap - - - - - -\n" + next_usa_gm_name + " - " + next_usa_map_name
                 return stringtext
             else:
                 print("Results not available in the JSON response.")
